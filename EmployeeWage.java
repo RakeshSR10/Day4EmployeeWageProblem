d5943336e29c946825353f782df1993355c47cac
RakeshSR10 <rakeshsrking@gmail.com>
2021-04-05 10:21:25 +0530

    [Modified] added Maximum hours to EmployeeWage.java

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,20 +1,24 @@
 Welcome to Employee Wage Computation Program
 public class EmployeeWage 
 {
+{
+
 	//Constants
 	public static final	int IS_PART_TIME = 1;
 	public static final	int IS_FULL_TIME = 2;
 	public static final	int EMP_PER_RATE_HOUR = 20;
 	public static final	int NUM_OF_WORKING_DAYS = 20;
+	public static final	int MAX_HRS_IN_MONTH = 100;
 
 	public static void main(String[] args) 
 	{
 		//Variables
-		int empHrs=0, empWage=0, totalEmpWage=0;
+		int empHrs=0, totalEmpWage=0, totalEmpHrs=0, totalWorkingDays=0;
 
-		//Computation with switch and for loop
-		for(int day=0; day<NUM_OF_WORKING_DAYS; day++)
+		//Computation and total working days per month
+		while(totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 		{
+			totalWorkingDays++;	
 			int empCheck =(int) Math.floor(Math.random() *10) % 3;
 			switch(empCheck)
 			{
@@ -27,11 +31,11 @@ public class EmployeeWage
 			default:
 				empHrs=0;
 			}
+
+			totalEmpHrs += empHrs;
+			System.out.println("Days = "+totalWorkingDays+ ", Employee Hours = "+empHrs);
 		}
-		empWage = empHrs * EMP_PER_RATE_HOUR;
-		totalEmpWage =empWage;
-		System.out.println("Total Employee wage = "+totalEmpWage);
+		totalEmpWage = totalEmpHrs * NUM_OF_WORKING_DAYS;
+		System.out.println("Total Employee Wage = "+totalEmpWage);
 	}
-
-
 }
